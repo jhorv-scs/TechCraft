20b6d207e943de7d3e1a0899a80a60642a1f2d7a
jacoo2 <jacoo2@localhost>
2011-04-22 14:27:08 +0200

    [Updated] Code formatting

--- TechCraft/NewTake/NewTake/view/multithreadLighting/VertexBuildChunkProcessor.cs
+++ TechCraft/NewTake/NewTake/view/multithreadLighting/VertexBuildChunkProcessor.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿#region license
+
+//  TechCraft - http://techcraft.codeplex.com
+//  This source code is offered under the Microsoft Public License (Ms-PL) which is outlined as follows:
+
+//  Microsoft Public License (Ms-PL)
+//  This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+
+//  1. Definitions
+//  The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+//  A "contribution" is the original software, or any additions or changes to the software.
+//  A "contributor" is any person that distributes its contribution under this license.
+//  "Licensed patents" are a contributor's patent claims that read directly on its contribution.
+
+//  2. Grant of Rights
+//  (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+//  (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+
+//  3. Conditions and Limitations
+//  (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+//  (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+//  (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+//  (D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+//  (E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement. 
+#endregion
+
+#region using
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 using NewTake.model;
 using NewTake.view.blocks;
 using NewTake.profiling;
-
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+#endregion
 
 namespace NewTake.view.blocks
 {
@@ -23,6 +50,7 @@ namespace NewTake.view.blocks
             _graphicsDevice = graphicsDevice;
         }
 
+        #region BuildVertexList
         private void BuildVertexList(Chunk chunk)
         {
             //lowestNoneBlock and highestNoneBlock come from the terrain gen (Eventually, if the terraingen did not set them you gain nothing)
@@ -68,7 +96,9 @@ namespace NewTake.view.blocks
 
             chunk.dirty = false;
         }
+        #endregion
 
+        #region BuildBlockVertexList
         private void BuildBlockVertexList(Block block, Chunk chunk, Vector3i chunkRelativePosition)
         {
 
@@ -298,9 +328,10 @@ namespace NewTake.view.blocks
 
                 BuildFaceVertices(chunk, blockPosition, chunkRelativePosition, BlockFaceDirection.ZIncreasing, block.Type, sunTL, sunTR, sunBL, sunBR, localTL, localTR, localBL, localBR);
             }
-
         }
+        #endregion
 
+        #region BuildPlantVertexList
         private void BuildPlantVertexList(Block block, Chunk chunk, Vector3i chunkRelativePosition)
         {
 
@@ -385,7 +416,9 @@ namespace NewTake.view.blocks
 
             BuildPlantVertices(chunk, blockPosition, chunkRelativePosition, block.Type, 0.6f, Color.LightGray);
         }
+        #endregion
 
+        #region BuildPlantVertices
         public void BuildPlantVertices(Chunk chunk, Vector3i blockPosition, Vector3i chunkRelativePosition, BlockType blockType, float sunLight, Color localLight)
         {
             BlockTexture texture = BlockInformation.GetTexture(blockType);
@@ -419,7 +452,9 @@ namespace NewTake.view.blocks
             AddVertex(chunk, blockPosition, chunkRelativePosition, new Vector3(0, 0, 0.5f), new Vector3(0, 0, -1), UVList[5], sunLight, localLight);
             AddIndex(chunk, 0, 1, 2, 2, 1, 3);
         }
+        #endregion
 
+        #region BuildFaceVertices
         public void BuildFaceVertices(Chunk chunk, Vector3i blockPosition, Vector3i chunkRelativePosition, BlockFaceDirection faceDir, BlockType blockType, float sunLightTL, float sunLightTR, float sunLightBL, float sunLightBR, Color localLightTL, Color localLightTR, Color localLightBL, Color localLightBR)
         {
             BlockTexture texture = BlockInformation.GetTexture(blockType, faceDir);
@@ -497,12 +532,14 @@ namespace NewTake.view.blocks
                     break;
             }
         }
+        #endregion
 
         private void AddVertex(Chunk chunk, Vector3i blockPosition, Vector3i chunkRelativePosition, Vector3 vertexAdd, Vector3 normal, Vector2 uv1, float sunLight, Color localLight)
         {
             chunk.vertexList.Add(new VertexPositionTextureLight(blockPosition.asVector3() + vertexAdd, uv1, sunLight, localLight.ToVector3()));
         }
 
+        #region AddIndex
         private void AddIndex(Chunk chunk, short i1, short i2, short i3, short i4, short i5, short i6)
         {
             chunk.indexList.Add((short)(chunk.VertexCount + i1));
@@ -513,6 +550,7 @@ namespace NewTake.view.blocks
             chunk.indexList.Add((short)(chunk.VertexCount + i6));
             chunk.VertexCount += 4;
         }
+        #endregion
 
         public void InitChunk(Chunk chunk)
         {
