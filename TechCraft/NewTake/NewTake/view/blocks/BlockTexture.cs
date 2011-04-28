b64d7d04fd8ac6c9d6fb8290b94b1125123eb330
stevem <stevem@SteveMarshall.RAP>
2011-04-28 09:55:08 +0100

    Increased size of texture library to 16x16 blocks

--- TechCraft/NewTake/NewTake/view/blocks/BlockTexture.cs
+++ TechCraft/NewTake/NewTake/view/blocks/BlockTexture.cs
@@ -63,32 +63,15 @@ namespace NewTake.view.blocks
 
     public static class TextureHelper
     {
-        public const int TEXTUREATLASSIZE = 8;
-        public const int CRACKATLASSIZE = 3;
+        public const int TEXTUREATLASSIZE = 16;
 
         public static Dictionary<int, Vector2[]> UVMappings;
-        public static Dictionary<int, Vector2[]> CrackMappings;
 
         static TextureHelper()
         {
             BuildUVMappings();
-            BuildCrackMappings();
         }
 
-        private static Dictionary<int, Vector2[]> BuildCrackMappings()
-        {
-            CrackMappings = new Dictionary<int, Vector2[]>();
-            for (int i = 0; i < 256; i++)
-            {
-                CrackMappings.Add((i * 6), TextureHelper.GetCrackMapping(i, BlockFaceDirection.XIncreasing));
-                CrackMappings.Add((i * 6) + 1, TextureHelper.GetCrackMapping(i, BlockFaceDirection.XDecreasing));
-                CrackMappings.Add((i * 6) + 2, TextureHelper.GetCrackMapping(i, BlockFaceDirection.YIncreasing));
-                CrackMappings.Add((i * 6) + 3, TextureHelper.GetCrackMapping(i, BlockFaceDirection.YDecreasing));
-                CrackMappings.Add((i * 6) + 4, TextureHelper.GetCrackMapping(i, BlockFaceDirection.ZIncreasing));
-                CrackMappings.Add((i * 6) + 5, TextureHelper.GetCrackMapping(i, BlockFaceDirection.ZDecreasing));
-            }
-            return CrackMappings;
-        }
 
         private static Dictionary<int, Vector2[]> BuildUVMappings()
         {
@@ -105,91 +88,6 @@ namespace NewTake.view.blocks
             return UVMappings;
         }
 
-        public static Vector2[] GetHiresMapping()
-        {
-            Vector2[] UVList = new Vector2[6];
-            UVList[0] = new Vector2(0, 0);
-            UVList[1] = new Vector2(1, 0);
-            UVList[2] = new Vector2(0, 1);
-            UVList[3] = new Vector2(0, 1);
-            UVList[4] = new Vector2(1, 0);
-            UVList[5] = new Vector2(1, 1);
-            return UVList;
-        }
-
-        #region GetCrackMapping
-        public static Vector2[] GetCrackMapping(int texture, BlockFaceDirection faceDir)
-        {
-            int textureIndex = texture;
-            // Assumes a Crack atlas of 3x3 textures
-
-            int y = textureIndex / CRACKATLASSIZE;
-            int x = textureIndex % CRACKATLASSIZE;
-
-            float ofs = 1f / 3f;
-
-            float yOfs = y * ofs;
-            float xOfs = x * ofs;
-
-            //ofs -= 0.01f;
-
-            Vector2[] UVList = new Vector2[6];
-
-            switch (faceDir)
-            {
-                case BlockFaceDirection.XIncreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[1] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[2] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[3] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[5] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    break;
-                case BlockFaceDirection.XDecreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[1] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[2] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    UVList[3] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    UVList[5] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    break;
-                case BlockFaceDirection.YIncreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[1] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[2] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[3] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[5] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    break;
-                case BlockFaceDirection.YDecreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[1] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[2] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[3] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[5] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    break;
-                case BlockFaceDirection.ZIncreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[1] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[2] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    UVList[3] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    UVList[5] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    break;
-                case BlockFaceDirection.ZDecreasing:
-                    UVList[0] = new Vector2(xOfs, yOfs);                // 0,0
-                    UVList[1] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[2] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[3] = new Vector2(xOfs, yOfs + ofs);          // 0,1
-                    UVList[4] = new Vector2(xOfs + ofs, yOfs);          // 1,0
-                    UVList[5] = new Vector2(xOfs + ofs, yOfs + ofs);    // 1,1
-                    break;
-            }
-            return UVList;
-        }
-        #endregion
-
         #region GetUVMapping
         public static Vector2[] GetUVMapping(int texture, BlockFaceDirection faceDir)
         {
@@ -199,7 +97,7 @@ namespace NewTake.view.blocks
             int y = textureIndex / TEXTUREATLASSIZE;
             int x = textureIndex % TEXTUREATLASSIZE;
 
-            float ofs = 1f / 8f;
+            float ofs = 1f / ((float)TEXTUREATLASSIZE);
 
             float yOfs = y * ofs;
             float xOfs = x * ofs;
